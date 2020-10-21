0372130bee0dd5775bea26621a4ae3fc659f4f9e
rouqiu <rouqiu@qunhemail.com>
2020-10-21 17:33:06 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from sklearn import linear_model
+
+
+if __name__ == '__main__':
+    reg = linear_model.LinearRegression()
+    reg.fit([[0,0], [1,1], [2,2]], [0,1,2])
+    print(reg.coef_)
+    print(reg.intercept_)
+
